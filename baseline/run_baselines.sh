1f5b7e16380c03d32acf678d7b58f90b236b6142
dorothy <55689863+shonyeajin@users.noreply.github.com>
2026-09-11 09:32:47 +0900

    Create run_baselines.sh

---
+++ baseline/run_baselines.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Example script for running open-source baselines
+echo "Refer to baseline_instructions.md for full details."
+echo "Running DeepDISOBind..."
+# python run_deepdisobind.py --data ../data/processed/DP81
